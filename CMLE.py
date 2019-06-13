7790f7a146ec27581909f97e6371119f0e2b1d4d
mmugnier31 <33942081+mmugnier31@users.noreply.github.com>
2019-06-13 15:03:39 +0200

    Update CMLE.py
    add LR test and BFGS solver

--- CMLE.py
+++ CMLE.py
@@ -18,6 +18,7 @@ References
 import numpy as np
 from sympy.utilities.iterables import multiset_permutations
 from scipy.special import comb
+from scipy.optimize import fmin_l_bfgs_b
 
 class BinLogitCMLE():
     """
@@ -43,8 +44,7 @@ class BinLogitCMLE():
         else:
             self.n, self.T, self.K = A.shape
         
-        self.R = self.compute_perm()
-        
+        self.R = self.compute_perm()   
         
     def compute_perm(self):
         """
@@ -62,7 +62,7 @@ class BinLogitCMLE():
             R.append(array)
         return R
 
-    def objective_i(self, i, beta):
+    def objective_i(self, i, A, b, beta):
         """
         Returns the i-th contribution to the objective function (CMLE is a finite-sum).
         
@@ -71,20 +71,36 @@ class BinLogitCMLE():
         `A' : design matrix
         `b' : outcome variable
         """
-        A = self.A
-        b = self.b
         R = self.R
         Xprime_beta = A[i].dot(beta)
         n_i = int(np.sum(b[i]))
-        omega_i = (1. / (np.sum(np.exp(R[n_i].dot(Xprime_beta)))))
-        return np.log(np.exp(np.dot(b[i], Xprime_beta)) * omega_i)
+        res = 0
+        if((n_i!=0) & (n_i!=self.T)):
+            omega_i = (1. / (np.sum(np.exp(R[n_i].dot(Xprime_beta)), axis=0)))
+            res = np.log(np.exp(np.dot(b[i], Xprime_beta)) * omega_i)
+        return res
     
-    def objective(self, beta):
+    def objective(self, beta, A, b):
         objective = 0
         for i in range(self.n):
-            objective += self.objective_i(i, beta)
+            objective += self.objective_i(i, A, b, beta)
         return objective / self.n
-            
+    
+    def loss(self, beta, A, b):
+        objective = 0
+        for i in range(self.n):
+            objective += self.objective_i(i, A, b, beta)
+        return - objective / self.n
+    
+    def loss_grad(self, beta, A, b):
+        """
+        Computes the global gradient.
+        """
+        g = np.zeros_like(beta)
+        for i in range(self.n):
+            g += self.comp_grad_i(i, beta)
+        return - g / self.n
+    
     def comp_grad_i(self, i, beta): 
         """
         Returns the pointwise gradient evaluated on datum i.
@@ -96,16 +112,17 @@ class BinLogitCMLE():
         Xprime_beta = A[i].dot(beta)
         if((n_i!=0) & (n_i!=self.T)):
             omega_i = (1. / (np.sum(np.exp(R[n_i].dot(Xprime_beta)))))
-            g = (np.dot(b[i], A[i]) - (np.sum(np.dot(R[n_i], A[i]) * 
-                        np.array([np.exp(R[n_i].dot(Xprime_beta)), 
-                                  np.exp(R[n_i].dot(Xprime_beta))]).T, axis=0)) * omega_i)
+            g = (np.dot(b[i], A[i]) - np.sum(np.dot(R[n_i], A[i]) * 
+                        np.tile(np.exp(R[n_i].dot(Xprime_beta)), 
+                                (self.K, 1)).T, axis=0) * omega_i)
         else: # case where no matrix but a unique vector in R[n_i]
             omega_i = (1. / (np.sum(np.exp(R[n_i][0].dot(Xprime_beta)))))
-            g = (np.dot(b[i], A[i]) - (np.sum(np.dot(R[n_i][0], A[i]) * 
-                        np.array([np.exp(R[n_i][0].dot(Xprime_beta)), 
-                                  np.exp(R[n_i][0].dot(Xprime_beta))]).T, axis=0)) * omega_i)
+            #print(omega_i)
+            g = (np.dot(b[i], A[i]) - np.sum(np.dot(R[n_i][0], A[i]) * 
+                        np.tile(np.exp(R[n_i][0].dot(Xprime_beta)), 
+                                (self.K, 1)).T[0], axis=0) * omega_i)
         return g
-    
+       
     def comp_hessian_i(self, i, beta):
         """
         Returns the pointwise Hessian matrix evaluated on datum i.
@@ -118,25 +135,21 @@ class BinLogitCMLE():
         if((n_i!=0) & (n_i!=self.T)):
             omega_i = (1. / (np.sum(np.exp(R[n_i].dot(Xprime_beta)))))
             b_i = np.sum(np.dot(R[n_i], A[i]) * 
-                        np.array([np.exp(R[n_i].dot(Xprime_beta)), 
-                                  np.exp(R[n_i].dot(Xprime_beta))]).T, axis=0)
-            #print(b_i)
+                        np.tile(np.exp(R[n_i].dot(Xprime_beta)), (self.K, 1)).T, axis=0)
             hess = (np.outer(b_i, b_i) * omega_i ** 2 - 
                     np.sum([np.outer(elem, elem) for idx, elem in enumerate(np.dot(R[n_i], A[i]))]) 
                     * omega_i)
         else:
             omega_i = (1. / (np.sum(np.exp(R[n_i].dot(Xprime_beta)))))
             b_i = np.sum(np.dot(R[n_i][0], A[i]) * 
-                        np.array([np.exp(R[n_i][0].dot(Xprime_beta)), 
-                                  np.exp(R[n_i][0].dot(Xprime_beta))]).T, axis=0)
+                        np.tile(np.exp(R[n_i][0].dot(Xprime_beta)), (self.K, 1)).T[0], axis=0)
             #print(b_i)
             hess = (np.outer(b_i, b_i) * omega_i ** 2 - 
                     np.sum([np.outer(elem, elem) for idx, elem in enumerate(np.dot(R[n_i][0], A[i]))]) 
                     * omega_i)
-            #hess = np.ndarray(shape=(K,K), buffer=np.zeros(K**2))
         return hess
 
-    def comp_gradient(self, beta):
+    def comp_gradient(self, beta, A, b):
         """
         Computes the global gradient.
         """
@@ -145,7 +158,7 @@ class BinLogitCMLE():
             g += self.comp_grad_i(i, beta)
         return g / self.n
 
-    def comp_hessian(self, beta):
+    def comp_hessian(self, beta): # warning : one-dimensional case
         """Computes the global Hessian.
         """
         hess = np.ndarray(shape=(self.K,self.K), buffer=np.zeros(self.K**2))
@@ -153,26 +166,30 @@ class BinLogitCMLE():
             hess += self.comp_hessian_i(i, beta)
         return hess / self.n
 
-    def fit(self, beta_init, n_iter, step=0.01, epsilon=1e-10, hessian=True):
+    def fit(self, beta_init, n_iter, step=0.01, epsilon=1e-10, hessian=False, verbose=True, BFGS=True):
         beta = beta_init.copy()
         beta_list = [beta_init]
-        if not hessian:
+        if BFGS:
+            beta, f_min, _ = fmin_l_bfgs_b(self.loss, beta_init, self.loss_grad, args=(self.A, self.b), pgtol=1e-6, factr=1e-30)
+        elif not hessian:
             stop = True
             for t in range(n_iter):
                 if stop:
                     beta = beta - step * self.comp_gradient(beta)
-                    print("Iteration %s completed" % t)
+                    if verbose:
+                        print("Iteration %s completed" % t)
                     beta_list.append(beta)
-                    if self.objective(beta) -self.objective(beta_list[t]) < epsilon:
+                    if self.objective(self.A, self.b, beta) -self.objective(self.A, self.b, beta_list[t]) < epsilon:
                         stop = False
         else:
             stop = True
             for t in range(n_iter):
                 if stop:
                     beta = beta - np.linalg.inv(self.comp_hessian(beta)).dot(self.comp_gradient(beta))
-                    print("Iteration %s completed" % t)
+                    if verbose:
+                        print("Iteration %s completed" % t)
                     beta_list.append(beta)
-                    if self.objective(beta) -self.objective(beta_list[t]) < epsilon:
+                    if self.objective(self.A, self.b, beta) -self.objective(self.A, self.b, beta_list[t]) < epsilon:
                         stop = False
         return beta, beta_list
     
@@ -186,3 +203,8 @@ class BinLogitCMLE():
             for i in range(self.n):
                 avar += - self.comp_hessian_i(i, beta) # Fix the minus -> get negative variance
         return np.linalg.inv(avar)
+    
+    def lr_nulltest(self, bet_hat):
+        lu = self.objective(self.A, self.b, bet_hat)
+        lr = self.objective(self.A, self.b, np.zeros(self.K))
+        return 2 * (lu - lr)
