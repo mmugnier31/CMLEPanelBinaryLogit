baaef2e690dc670c4ba243949fb7c7222f4e42e0
mmugnier31 <33942081+mmugnier31@users.noreply.github.com>
2020-06-25 19:18:34 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 20 08:52:37 2019
-
-@author: Martin
-"""
-
-"""
 Examples code for Conditional Maximum Likelihood Estimation (C.M.L.E.) in the binary logistic 
 model with unobserved heterogeneity.
 """
@@ -14,41 +8,47 @@ model with unobserved heterogeneity.
 #import os
 #os.chdir("setpath") 
 
+from matplotlib import pyplot as plt
 import numpy as np
 from CMLE import BinLogitCMLE
 
-from matplotlib import pyplot as plt
+np.random.seed(12)
 
-# Logistic model : T = 3, standard normal fixed effect and two random binary covariates
 def err_cdf(x):
+    '''Logistic model : T = 3, standard normal fixed effect and one random 
+    binary covariate.'''
     return 1 / (1 + np.exp(-x))
 
-def simulate_twobinvar(n, T, beta_0=np.array([1., 1.])):
-    K = 2
+def simulate_onebinvar(n, T, beta_0):
+    K = 1
     W = np.ndarray(shape=(n, T, K)) # explanatory variables
-    for raw in range(n):
+    for row in range(n):
         for period in range(T):
-            W[raw, period] = np.random.binomial(1, 0.5, size=2)
-            #W[raw, period] = np.random.normal(1, 0.5, size=2)
+            W[row, period] = np.random.binomial(1, 0.5, size=K)
     Y = np.ndarray(shape=(n,3)) # outcome variable
     for i in range(n):
-        Y[i] = np.random.binomial(1, err_cdf(np.dot(W[i],beta_0) + np.random.normal(0,1)), size = T)
-
+        fe = np.random.normal(0,1)
+        Y[i,:] = np.array([float(np.random.binomial(1, err_cdf(np.dot(W[i,j], beta_0) + fe))) for j in range(3)])
     return W, Y
 
-beta_0 = np.array([1., 1])
-W, Y = simulate_twobinvar(10000, 3)
-
+W, Y = simulate_onebinvar(10000, 3, np.array([1.]))
 model = BinLogitCMLE(A= W, b = Y)
 
-# Run CMLE with constant step
-beta_min, beta_list = model.fit(beta_init=np.zeros(2), n_iter=100, step=0.1, epsilon = 1e-10, hessian=False)
-plt.plot([model.objective(elem) for elem in beta_list])
-plt.show()
-print("CMLE estimator : %s" % beta_min) # convergence seems OK
 
-# Run CMLE with hessian step
-beta_min, beta_list = model.fit(beta_init=np.zeros(2), n_iter=100, step=0.1, epsilon = 1e-10, hessian=True)
-plt.plot([model.objective(elem) for elem in beta_list])
+# Run CMLE with constant step
+beta_min, beta_list = model.fit(beta_init=np.zeros(1), n_iter=100, step=0.1, epsilon = 1e-10, hessian=False, BFGS=False)
+plt.plot([model.objective(elem, model.A, model.b) for elem in beta_list])
 plt.show()
-print("CMLE estimator : %s" % beta_min) # convergence seems OK
+print("CMLE estimator (Raphson-Newton with constant step) : %s" % beta_min) # convergence is OK but slow
+
+# Run CMLE with constant step
+beta_min, beta_list = model.fit(beta_init=np.zeros(1), n_iter=100, step=0.1, epsilon = 1e-10, hessian=True, BFGS=False)
+plt.plot([model.objective(elem, model.A, model.b) for elem in beta_list])
+plt.show()
+print("CMLE estimator (Raphson-Newton with hessian step) : %s" % beta_min) # convergence in one step
+
+# Run CMLE with L-BFGS-B
+beta_min, beta_list = model.fit(beta_init=np.zeros(1), n_iter=100, step=0.1, epsilon = 1e-10, hessian=False)
+plt.plot([model.objective(elem, model.A, model.b) for elem in beta_list])
+plt.show()
+print("CMLE estimator (L-BFGS-B) : %s" % beta_min) # convergence is faster
