baec13403fc3f9c9120484274554d3433c6c7200
mmugnier31 <33942081+mmugnier31@users.noreply.github.com>
2020-06-25 19:01:08 +0200

    Update CMLE.py

--- CMLE.py
+++ CMLE.py
@@ -1,17 +1,65 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon May 20 08:52:37 2019
+%------------------------- CMLE ---------------------------------------------
 
-@author: Martin
-"""
+ Paper : Identification W/O Logit Errors - Simulations 
+ Author : Martin MUGNIER
+ Version : 25/06/2020
 
-"""
-Conditional Maximum Likelihood Estimation (C.M.L.E.) in the binary logistic 
-model with unobserved heterogeneity.
 
-References
+ This code implements the Conditional Maximum Likelihood Estimator (C.M.L.E.) 
+ for the (normalized) slope parameter in a panel binary logistic model with 
+ unobserved heterogeneity.
+
+%----------------------------------------------------------------------------
+
+ References :
 ----------
  Woolridge, "Econometric Analysis of Cross-Section and Panel Data" (Chap 15.8.3).
+ 
+ Example :
+----------
+np.random.seed(12)
+from matplotlib import pyplot as plt
+
+def err_cdf(x):
+    '''Logistic model : T = 3, standard normal fixed effect and one random 
+    binary covariate.'''
+    return 1 / (1 + np.exp(-x))
+
+def simulate_onebinvar(n, T, beta_0):
+    K = 1
+    W = np.ndarray(shape=(n, T, K)) # explanatory variables
+    for row in range(n):
+        for period in range(T):
+            W[row, period] = np.random.binomial(1, 0.5, size=K)
+    Y = np.ndarray(shape=(n,3)) # outcome variable
+    for i in range(n):
+        fe = np.random.normal(0,1)
+        Y[i,:] = np.array([float(np.random.binomial(1, err_cdf(np.dot(W[i,j], beta_0) + fe))) for j in range(3)])
+    return W, Y
+
+W, Y = simulate_onebinvar(10000, 3, np.array([1.]))
+model = BinLogitCMLE(A= W, b = Y)
+
+
+# Run CMLE with constant step
+beta_min, beta_list = model.fit(beta_init=np.zeros(1), n_iter=100, step=0.1, epsilon = 1e-10, hessian=False, BFGS=False)
+plt.plot([model.objective(elem, model.A, model.b) for elem in beta_list])
+plt.show()
+print("CMLE estimator (Raphson-Newton with constant step) : %s" % beta_min) # convergence is OK but slow
+
+# Run CMLE with constant step
+beta_min, beta_list = model.fit(beta_init=np.zeros(1), n_iter=100, step=0.1, epsilon = 1e-10, hessian=True, BFGS=False)
+plt.plot([model.objective(elem, model.A, model.b) for elem in beta_list])
+plt.show()
+print("CMLE estimator (Raphson-Newton with hessian step) : %s" % beta_min) # convergence in one step
+
+# Run CMLE with L-BFGS-B
+beta_min, beta_list = model.fit(beta_init=np.zeros(1), n_iter=100, step=0.1, epsilon = 1e-10, hessian=False)
+plt.plot([model.objective(elem, model.A, model.b) for elem in beta_list])
+plt.show()
+print("CMLE estimator (L-BFGS-B) : %s" % beta_min) # convergence is faster
 
 """
 
@@ -176,21 +224,21 @@ class BinLogitCMLE():
             stop = True
             for t in range(n_iter):
                 if stop:
-                    beta = beta - step * self.comp_gradient(beta)
+                    beta = beta + step * self.comp_gradient(beta, self.A, self.b)
                     if verbose:
                         print("Iteration %s completed" % t)
                     beta_list.append(beta)
-                    if self.objective(self.A, self.b, beta) -self.objective(self.A, self.b, beta_list[t]) < epsilon:
+                    if self.objective(beta, self.A, self.b, ) -self.objective( beta_list[t], self.A, self.b,) < epsilon:
                         stop = False
         else:
             stop = True
             for t in range(n_iter):
                 if stop:
-                    beta = beta - np.linalg.inv(self.comp_hessian(beta)).dot(self.comp_gradient(beta))
+                    beta = beta - np.linalg.inv(self.comp_hessian(beta)).dot(self.comp_gradient(beta, self.A, self.b))
                     if verbose:
                         print("Iteration %s completed" % t)
                     beta_list.append(beta)
-                    if self.objective(self.A, self.b, beta) -self.objective(self.A, self.b, beta_list[t]) < epsilon:
+                    if self.objective(beta, self.A, self.b) -self.objective(beta_list[t], self.A, self.b) < epsilon:
                         stop = False
         return beta, beta_list
     
@@ -202,8 +250,8 @@ class BinLogitCMLE():
                 avar += np.outer(grad, grad)
         else:
             for i in range(self.n):
-                avar += - self.comp_hessian_i(i, beta) # Fix the minus -> get negative variance
-        return np.linalg.inv(avar)
+                avar += - self.comp_hessian_i(i, beta) # Fix the minus ? -> get negative variance
+        return np.linalg.inv(avar / self.n)
     
     def lr_nulltest(self, bet_hat, lvl=0.05):
          """
@@ -211,8 +259,8 @@ class BinLogitCMLE():
          ----------
          Outputs : LR statistics, p-value
          """
-         lu = self.objective(self.A, self.b, bet_hat)
-         lr = self.objective(self.A, self.b, np.zeros(self.K))
+         lu = self.objective(bet_hat, self.A, self.b)
+         lr = self.objective(np.zeros(self.K), self.A, self.b)
          ratio = 2 *self.n * (lu - lr)
          pval = stats.chi2.sf(ratio, self.K)
          if pval<lvl:
